d3c234e740a8839aa774ed86295e9a59aa516196
Aaron Orellana <aaronorellana1@gmail.com>
2018-04-02 22:47:35 -0400

    Ajout des fichiers font-awesome
    Ajout du wp_register_style pour le font awesome.

--- functions.php
+++ functions.php
@@ -5,11 +5,13 @@ function lapizzeria_styles() {
     //Registar los estilos
     //               Nom du style                URL                                      DEPS    VERSION
     wp_register_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.0' );
+    wp_register_style( 'fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css', array('normalize'), '4.7.0' );
                                                                                                // el archivo normalize se va realisar en primero
     wp_register_style( 'style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0' );
 
     //llamar a los estilos
     wp_enqueue_style('normalize');
+    wp_enqueue_style('fontawesome');
     wp_enqueue_style('style');
 
 }
