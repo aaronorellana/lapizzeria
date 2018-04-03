203f6f0caa6fd21134711a108dccab753454d95f
Aaron Orellana <aaronorellana1@gmail.com>
2018-04-02 22:47:57 -0400

    Ajout du lien pour google font - header.php

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Ex</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <title>Pizzeria</title>
         <?php wp_head(); ?>
     </head>
 <body>
