152a65b1f3cc28097e466a9be73dbc1cb7788fab
Aaron Orellana <aaronorellana1@gmail.com>
2018-04-16 09:48:17 -0400

    Add footer - footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,25 @@
+        
+        <footer>
+            <?php 
+                // Arguments pour le menu
+                $args = array(
+                    'theme_location' => 'header-menu',
+                    'container' => 'nav',
+                    'after' => '<span class"separador"> | </span>',
+                );
+
+                // Function pour print le menu
+                wp_nav_menu( $args );
+            ?>
+
+            <div class="ubicacion">
+                <p>8908 Rue Marco, California , USA</p>
+                <p>Tel: 33-33-22-11</p>
+            </div>
+
+            <p class="copyright">Todos los derechos reservados <?php echo date('Y')?> </p>
+        </footer>
+        
         <?php wp_footer(); ?>
     </body>
 </html>
