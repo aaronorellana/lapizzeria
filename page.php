85a03d52b4cf3b1fc553ff3798b4134d248cab1c
Aaron Orellana <aaronorellana1@gmail.com>
2018-04-16 09:50:59 -0400

    Creation de la structure pour les pages.

--- page.php
+++ page.php
@@ -1,5 +1,27 @@
 <?php get_header(); ?>
+
+    <!-- Debut de la boucle while -->
+    <?php while(have_posts()): the_post(); ?> 
+
+        <!-- Thumbnail -->
+
+
+        <div class="hero" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>);">
+            <div class="contenido-hero">
+                <div class="texto-hero">
+                     <!-- Titre de la page-->
+                     <?php the_title('<h1>', '</h1>'); ?>
+                </div>
+            </div>
+        </div>
     
-    <h1>Hola desde page.php</h1>
+        <!-- Contenue -->
+        <div class="principal contenedor">
+            <main class="text-centrado contenido-paginas">
+                <?php the_content(); ?>
+            </main>
+        </div>
+
+    <?php endwhile; ?> <!-- Fin de la boucle while -->
 
 <?php get_footer(); ?>
